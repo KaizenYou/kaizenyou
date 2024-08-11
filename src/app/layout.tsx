324c968d8aeec1f6fdf45259f99feeae8ad6ec23
Kritika Singh <101945124+Kritikasingh2004@users.noreply.github.com>
2024-08-11 22:58:42 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/kaizenyou.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <ClientSessionProvider>
           <SmoothScroll>{children}</SmoothScroll>
